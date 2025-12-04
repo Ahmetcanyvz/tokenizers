c2482ad61bd19dcd56878c57f918db4480804d04
Ahmetcan Yavuz <ayavuz@student.ethz.ch>
2025-12-05 00:34:21 +0100

    fix

--- fineweb_test.py
+++ fineweb_test.py
@@ -29,7 +29,7 @@ from typing import Iterable, List, Optional
 
 # ---------- CONFIG ----------
 DATASET_PATH = "/Users/ahmetcanyavuz/Developer/tokenizers/fineweb_data/fineweb_en_sentences.txt"
-RESULTS_DIR  = "./results_100k"
+RESULTS_DIR  = "./results_100k_fixed"
 VOCAB_SIZE   = 100_000
 MIN_FREQ     = 2
 
